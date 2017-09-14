69eadd07f88aa8e2800e802c8f61a63bbb4cc43d
ptnk1995 <nguyenphuoctiendn@gmail.com>
2017-09-14 17:56:55 +0700

    add view job

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
+  resources :jobs
+  resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
